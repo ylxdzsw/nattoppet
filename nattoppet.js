a12218cad0e4a7fc690b55d1327079a488d98d02
ylxdzsw <ylxdzsw@gmail.com>
2018-09-14 19:19:46 +0800

    reform

--- nattoppet.js
+++ nattoppet.js
@@ -6,73 +6,98 @@ const fs = require('fs')
 const path = require('path')
 const transformer = require('jstransformer')
 
-const pug    = transformer(require('jstransformer-pug'))
+const ymd    = transformer(require('jstransformer-ymd'))
 const coffee = transformer(require('jstransformer-coffee-script'))
 const scss   = transformer(require('jstransformer-scss'))
 const marked = transformer(require('jstransformer-marked'))
 const minify = transformer(require('jstransformer-html-minifier'))
 const uglify = transformer(require('jstransformer-uglify-js'))
 
-const vars = {
-    assetpath: path.join(__dirname, 'assets').replace(/C:\\|\\/g, '/') // workaround for pug on windows
+function move_match(state, reg) {
+    reg.lastIndex = state.i
+    const token = reg.exec(state.str)
+    state.i += token[0].length
+    return token
 }
 
-const compile = file => {
-    const suffix = path.extname(file).slice(1).toLowerCase()
-    const _require = x => compile(x.startsWith('/') ? x : path.join(path.dirname(file), x))
-    const content = () => {
-        let x = fs.readFileSync(file, 'utf8')
-        const list = x.match(/@nattoppet[:\.][a-zA-Z0-9\.\-_]+/g) || []
-        for (const token of list) {
-            const c = token.startsWith("@nattoppet:") ? _require(token.slice(11)) :
-                      token.startsWith("@nattoppet.") ? vars[token.slice(11)] : token
-            x = x.split(token).join(c) // replace all without buiding a proper escaped RegExp
-        }
-        return x
-    }
+const helpers = {
+    nattoppet_dir: __dirname,
 
-    switch (suffix) {
-        case 'pug':
-        case 'jade':
-            const x = pug.render(content(), {filename: file, basedir: '/'}, {require: _require}).body
-            return minify.render(x, { removeAttributeQuotes:true, removeComments:true }).body
-        case 'scss':
-            return scss.render(content(), {outputStyle: 'compressed'}).body
-        case 'coffee':
-            return uglify.render(coffee.render(content()).body).body
-        case 'md':
-            return marked.render(content()).body
-        case 'png':
-        case 'jpg':
-        case 'gif':
-        case 'jpeg':
-            return `data:image/${suffix};base64,` + fs.readFileSync(file, 'base64')
-        case 'blob':
-        case 'bin':
-            return "data:application/octet-stream;base64," + fs.readFileSync(file, 'base64')
-        default:
-            return content()
+    nattoppet_parse() {
+        const opts = [], args = [], blocks = []
+        const parse_option = () => {
+            const token = move_match(this.state, /\.([\w\-]+)/g)
+            opts.push(token[1])
+        }
+        const parse_argument = () => {
+            const token = move_match(this.state, /\(.*?\)|{.*?}/g)
+            args.push(token[1])
+        }
+        const parse_block = () => {
+            const token = move_match(this.state, />+/g)
+            blocks.push(this.capture_until('<'.repeat(token[0].length)))
+        }
+
+        while (true) {
+            switch (this.peek()) {
+                case '.':
+                    parse_option()
+                    break
+                case '(':
+                case '{':
+                    parse_argument()
+                    break
+                case '>':
+                    parse_block()
+                    break
+                default:
+                    return { opts, args, blocks }
+            }
+        }
+    },
+
+    include(file, type='utf8') {
+        if (file.startsWith('@nattoppet')) {
+            file = path.join(this.nattoppet_dir, file.substring(10))
+        } else if (!file.startsWith('/')) {
+            file = path.join(this.base_dir, file)
+        }
+        return fs.readFileSync(file, type)
+    },
+
+    render_coffee(str) {
+        return uglify.render(coffee.render(str).body).body
+    },
+
+    render_scss(str) {
+        return scss.render(str, {outputStyle: 'compressed'}).body
+    },
+
+    render_markdown(str) {
+        return marked.render(str).body
     }
+}
+
+function render_files(env, ...files) {
+    const str = files.map(x=>fs.readFileSync(x, 'utf8')).reduce((x, y) => x + y)
+    return ymd.render(str, null, env).body
+}
+
+const rpath = (...x) => path.join(__dirname, ...x)
+
+const render = file => {
+    const seg = path.basename(file).split('.')
+    const theme = seg[seg.length-2]
+    const raw = fs.readFileSync(file, 'utf8')
+    const html = ['koa', 'ppt', 'vue'].includes(theme)
+        ? render_files(rpath(theme, 'before.ymd'), file, rpath(theme, 'after.ymd'), rpath('nattoppet.ymd'))
+        : render_files(file, rpath('nattoppet.ymd'))
+    return minify.render(html, { removeAttributeQuotes:true, removeComments:true }).body
 }
 
 const file = process.argv[2]
-const opts = process.argv[3]
 
-if (process.argv.length < 3 || file == "--help") {
-    return console.log(`
-nattoppet: A tiny HTML bundler.
+if (process.argv.length != 3 || file == "--help")
+    return console.log("Usage: nattoppet file.{koa,ppt,vue}.ymd > file.html")
 
-  nattoppet file [opt.json] > file.html
-
-Compile file with a special function "require" which you can use inside the jade file. Required file will be \
-bundle into the compiled HTML file directly, with coffee/sass compiled and minified and images base64 encoded.
-
-Special replacements are performed for "@nattoppet:[file]" and "@nattoppet.[var]". The former will be replaced \
-by compiled string of that file, and the latter will be replaced by the content provided as in [opt.json].
-`)}
-
-if (opts) {
-    Object.assign(vars, JSON.parse(fs.readFileSync(file, 'utf8')))
-}
-
-process.stdout.write(compile(file))
+process.stdout.write(render(file))
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 Nattoppet
 =========
 
-A tiny HTML bundler and static blog generator for [my own blog](https://blog.ylxdzsw.com).
+Several themes for [ymd](https://github.com/ylxdzsw/ymd.js) that are used in [my blog](https://blog.ylxdzsw.com).
 
 #### Usage
 
 ```
 $ npm install -g nattoppet
-$ nattoppet file.jade
+$ nattoppet file.ppt.ymd
 ```
 
 for more information about nattoppet CLI util, use `nattoppet --help`.
